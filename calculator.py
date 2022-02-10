def28ce66c904e13d52cbd12281cc78715a4232d
Katherine Jiang <katherine.jiang30@gmail.com>
2022-02-10 04:47:20 +0000

    Cleaned up code

--- calculator.py
+++ calculator.py
@@ -7,69 +7,64 @@ while True:
     user_input = input("Enter your equation > ")
     tokens = user_input.split(" ")
 
-    if "q" in tokens or "quit" in tokens:
+    if "q" in tokens or "quit" in tokens:   # q to quit
         print("You will exit.")
         break
 
-    elif len(tokens) < 2:
+    if len(tokens) < 2:                     # needs minimum of 2 inputs (operator and number)
         print("Not enough inputs.")
         continue
 
     operator = tokens[0]
     num1 = tokens[1]
+    result = None                           # a place to store the results of the functions
 
     if len(tokens) < 3:
         num2 = "0"
-
     else:
         num2 = tokens[2]
 
     if len(tokens) > 3:
         num3 = tokens[3]
 
-    # A place to store the return value of the math function we call,
-    # to give us one clear place where that result is printed.
-    result = None
-
-    if not num1.isdigit() or not num2.isdigit():
+    if not num1.isdigit() or not num2.isdigit(): # check to see if digits are after operator
         print("Those aren't numbers!")
         continue
-
-    # We have to cast each value we pass to an arithmetic function from a
-    # a string into a numeric type. If we use float across the board, all
-    # results will have decimal points, so let's do that for consistency.
-
-    elif operator == "+":
-        result = add(float(num1), float(num2))
-
-    elif operator == "-":
-        result = subtract(float(num1), float(num2))
-
-    elif operator == "*":
-        result = multiply(float(num1), float(num2))
-
-    elif operator == "/":
-        result = divide(float(num1), float(num2))
-
-    elif operator == "square":
-        result = square(float(num1))
-
-    elif operator == "cube":
-        result = cube(float(num1))
-
-    elif operator == "pow":
-        result = power(float(num1), float(num2))
-
-    elif operator == "mod":
-        result = mod(float(num1), float(num2))
-
-    elif operator == "x+":
-        result = add_mult(float(num1), float(num2), float(num3))
-
-    elif operator == "cubes+":
-        result = add_cubes(float(num1), float(num2))
-
     else:
-        result = "Please enter an operator followed by two integers."
+        num1 = float(num1)                  # converts num1-3 as floats to pass into functions
+        num2 = float(num2)
+        num3 = float(num3)
 
-    print(result)
+        if operator == "+":
+            result = add(num1, num2)
+
+        elif operator == "-":
+            result = subtract(num1, num2)
+        
+        elif operator == "*":
+            result = multiply(num1, num2)
+
+        elif operator == "/":
+            result = divide(num1, num2)
+            
+        elif operator == "square":
+            result = square(num1)
+
+        elif operator == "cube":
+            result = cube(num1)
+        
+        elif operator == "pow":
+            result = power(num1, num2)        
+        
+        elif operator == "%":
+            result = mod(num1, num2)
+        
+        elif operator == "x+":
+            result = add_mult(num1, num2, num3)
+
+        elif operator == "cubes+":
+            result = add_cubes(num1, num2)
+        else:
+            result = "Please enter an operator followed by two integers."
+    
+        print(result)
